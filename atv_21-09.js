28cc5ed4b573fc0ab1a3c193a4d817f942f575d9
unknown <luiza.karlavc@gmail.com>
2022-09-21 18:00:24 -0300

    desafio If e Else If

---
+++ atv_21-09.js
@@ -0,0 +1,48 @@
+/* àlcool 
+até 25L, desconto de 2% por litro
+>25L, desconto de 4% por litro
+
+Gasolina 
+
+até 25L, desconto de 3% por litro
+>25L desconto de 5% por litro */
+
+const litros = parseFloat(prompt("Digite aqui a quantidade de litros vendidos"))
+console.log(litros)
+const msg = `
+Qual combustível foi usado?
+Digite A para Alcool 
+Digite G para Gasolina
+`
+const combustivel = prompt(msg)
+console.log(combustivel)
+
+const descontoA1 = 1.9 * 0.02
+const descontoA2 = 1.9 * 0.04
+
+const descontoG1 = 2.7 * 0.03
+const descontoG2 = 2.7 * 0.05
+
+
+if (combustivel == 'A') {
+    if (litros <= 25) {
+        const precoFinal = (1.9 * litros) - ( litros * descontoA1)
+        console.log(precoFinal)
+        alert(`O preço a ser pago será ${precoFinal.toFixed(2)} reais` )
+    } else if (litros > 25){
+        const precoFinal = (1.9 * litros) - (litros * descontoA2)
+        console.log(precoFinal)
+        alert(`O preço a ser pago será ${precoFinal.toFixed(2)}`)
+
+    }
+} else if (combustivel == 'G') {
+    if (litros <= 25) {
+    const precoFinal = (2.7 * litros) - (litros * descontoG1)
+    console.log(precoFinal)
+    alert(`O preço a ser pago será ${precoFinal.toFixed(2)}`)
+    } else if (litros > 25) {
+        const precoFinal = (2.7 * litros) - (litros * descontoG2)
+        console.log(precoFinal)
+        alert(`O preço a ser pago será ${precoFinal.toFixed(2)}`)
+    }
+}
---
+++ desafio21-09.js
@@ -0,0 +1,63 @@
+// Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado. 
+/**Código Condição de pagamento
+
+1 À vista em dinheiro ou cheque, recebe 10% de desconto
+2 À vista no cartão de crédito, recebe 15% de desconto
+3 Em duas vezes, preço normal de etiqueta sem juros
+4 Em três vezes, preço normal de etiqueta mais juros de 10% 
+*/
+
+/** 
+ * Métodos de pagamento
+ * -Dinheiro
+ * -Cheque
+ * -Cartão
+ */
+
+const precoProduto = parseFloat(prompt('Qual o preço do produto? (use . acrescentar os centavos)')) // Para aceitar decimais
+const msg = `
+Informe o méodo de pagamento:
+Digite 1 para pagamento em dinheiro
+Digite 2 para pagamento em cheque
+Digite 3 para pagamento em cartão
+`
+
+const pagamento = prompt(msg)
+
+console.log(precoProduto)
+console.log(pagamento)
+
+// 1 - À vista
+// if = Se ~ condição
+
+//Se o valor de pagamento for 1 ou 2 tem desconto de 10%
+if(pagamento == 1 || pagamento == 2) {
+    const precoFinal = precoProduto * 0.9
+    console.log(precoFinal)
+    alert(`O preço final do seu produto com desconto é ${precoFinal.toFixed(2)} reais`)
+
+    // .toFixed irá fixar a quantidade de casas decimais
+
+
+}
+// 2 - Cartão de crédito  1x 15% de desconto 2x sem desconto 3x> acréscimo de 10%
+else if (pagamento == 3) { // Else será considerado se o primeiro IF for falso
+    const parcelas = Number(prompt("Informe quantidade de parcelas"))
+
+    if (parcelas == 1){
+        const precoFinal = precoProduto * 0.85
+        console.log(precoFinal)
+        alert(`O preço final com desconto é ${precoFinal.toFixed(2)}.`)
+    } else if (parcelas == 2) {
+        alert(`O preço final é ${precoProduto} `)
+    } else if (parcelas >= 3) {
+        const precoFinal = precoProduto * 1.1
+        console.log(precoFinal)
+        alert(`O preço final do produto é ${precoFinal.toFixed(2)} reais. Será ${parcelas} parcelas de ${(precoFinal / parcelas).toFixed(2)} reais`)
+    }
+    
+    
+}
+
+
+
